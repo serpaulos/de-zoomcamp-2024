1aa3ed72f10be1232c14c5949d48eeaaab0efca4
Paulo Sergio da Silva Junior <paulosilvajrpt@gmail.com>
2024-02-21 19:37:04 +0000

    working with green model

--- week4_analytics_engineering/dbt_cloud/macros/get_payment_type_description.sql
+++ week4_analytics_engineering/dbt_cloud/macros/get_payment_type_description.sql
@@ -5,18 +5,12 @@
 
     case
         {{ payment_type }}
-        when 1
-        then 'Credit card'
-        when 2
-        then 'Cash'
-        when 3
-        then 'No charge'
-        when 4
-        then 'Dispute'
-        when 5
-        then 'Unknown'
-        when 6
-        then 'Voided trip'
+        when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
     end
 
 {%- endmacro %}
