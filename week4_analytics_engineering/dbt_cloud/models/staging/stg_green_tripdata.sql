ce54842751e66a07deb2b6ee8884d9e3d0fd8ed4
Paulo Sergio da Silva Junior <paulosilvajrpt@gmail.com>
2024-02-22 14:13:09 +0000

    testes

--- week4_analytics_engineering/dbt_cloud/models/staging/stg_green_tripdata.sql
+++ week4_analytics_engineering/dbt_cloud/models/staging/stg_green_tripdata.sql
@@ -1,5 +1,13 @@
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
+
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(["vendorid", "lpep_pickup_datetime"]) }} as tripid,
@@ -31,9 +39,13 @@ select
     {{ get_payment_type_description("payment_type") }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "green_tripdata")}}
-where vendorid is not null
--- dbt build -m <model.sql> --var 'is_test_run: false'
-{% if var("is_test_run", default=true) %} 
-    limit 100 
+from tripdata
+where rn = 1
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
 {% endif %}
--- week4_analytics_engineering/dbt_cloud/models/staging/stg_yellow_tripdata.sql
+++ week4_analytics_engineering/dbt_cloud/models/staging/stg_yellow_tripdata.sql
@@ -1,5 +1,13 @@
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_tripdata') }}
+  where vendorid is not null 
+)
+
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(["vendorid", "tpep_pickup_datetime"]) }} as tripid,
@@ -33,9 +41,12 @@ select
     {{ get_payment_type_description("payment_type") }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "yellow_tripdata")}}
-where vendorid is not null
--- dbt build -m <model.sql> --var 'is_test_run: false'
-{% if var("is_test_run", default=true) %} 
-    limit 100 
+from tripdata
+where rn = 1
+
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
 {% endif %}
