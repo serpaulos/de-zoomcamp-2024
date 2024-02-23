877370db22d8d9db308adab8b31c0922b974ea82
serpaulos <sergiopaulosilvajr@gmail.com>
2024-02-23 09:53:14 +0000

    modify models

--- week4_analytics_engineering/dbt_cloud/models/staging/stg_green_tripdata.sql
+++ week4_analytics_engineering/dbt_cloud/models/staging/stg_green_tripdata.sql
@@ -45,8 +45,8 @@ where rn = 1
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+--{% if var('is_test_run', default=true) %}
+--
+--  limit 100
+--
+--{% endif %}
